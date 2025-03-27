b6cef89a7fc3c1c258d492e398cd4683bbe7826d
Varshini Raja <99665788+VarshiniRaja@users.noreply.github.com>
2025-03-27 16:00:16 +0530

    Update chat_connect.py

--- chat_connect.py
+++ chat_connect.py
@@ -33,5 +33,9 @@ def receive_chat_data():
     }
     return jsonify(response), 200
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
